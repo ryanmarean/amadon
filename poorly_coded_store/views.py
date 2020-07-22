2da18edb9a2af8d238efd455b65683c098ac12c1
Ryan <ryan.marean@gmail.com>
2020-07-21 18:56:16 -0700

    Fixed error causing multiple orders on page refresh Fixed bug which allowed customer to set own price using html inspection

--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -9,8 +9,21 @@ def index(request):
 
 def checkout(request):
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
+    product_from_form = Product.objects.get(id=request.POST["product_id"])
+    price_from_form = float(product_from_form.price)
     total_charge = quantity_from_form * price_from_form
     print("Charging credit card...")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    return redirect('/order_confirm')
+
+def order_confirm(request):
+    all_orders = Order.objects.all()
+    rolling_total = 0
+    for order in all_orders:
+        rolling_total += order.total_price
+    context = {
+        "last_order" : Order.objects.last(),
+        "all_orders" : Order.objects.all(),
+        "total_orders" : rolling_total,
+    }
+    return render(request,"store/checkout.html",context)
